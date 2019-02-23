471ed2313de20c99d4221fb11751afaa0e13952b
hopeful2 <hopeful2@goandmake.app>
2019-02-23 16:38:14 +1100

    init demo on gitlab

---
+++ js/config.js
@@ -0,0 +1,16 @@
+window.gtpb = window.gtpb || {};
+
+
+
+     
+
+     
+
+
+gtpb.default.site_id = "gt";
+gtpb.default.SiteMenu = {"global":[{"URL":"/","Page":null,"Name":"Home","Menu":"global","Identifier":"tutorials","Pre":"","Post":"","Weight":5,"Parent":"","Children":null},{"URL":"/about/","Page":null,"Name":"About","Menu":"global","Identifier":"about","Pre":"","Post":"","Weight":15,"Parent":"","Children":null}],"iconmenu":[{"URL":"#","Page":null,"Name":"Go Account","Menu":"iconmenu","Identifier":"","Pre":"Go","Post":"","Weight":0,"Parent":"","Children":null}]};
+gtpb.default.SiteParams = {"accent_color":"hsl(165,35%,45%)","accent_color_light":"hsl(165,35%,65%)","custom_css":"\n@keyframes slowRightEntry {\n  0% {\n    transform: translateX(-100vw);\n    opacity: 0.3;\n  }\n50% {\n   opacity: .5;\n}\n\n  90% {\n   transform: translateX(0vw);\n   opacity: .8;  \n   }\n  100% {\n    transform: translateX(0vw);\n   opacity: 0.7;\n}\n}\n\n.slowRightEntry {\n  animation: slowRightEntry 10s linear 10;\n  height: 100%;\n  }\n\n\n@keyframes slowRightExit {\n  0% {\n    transform: translateX(0vw);\n   opacity: 1;\n  }\n\n  50% {\n    opacity: .1;\n }\n  100% {\n   opacity: .0;\n    transform: translateX(50vw);\n  }\n}\n\n.slowRightExit {\n  animation: slowRightExit 20s linear 5;\n  height: 100%;\n}\n","flex_box_interior_classes":"w-100 w-50-m w-33-l ","global_section":{},"gt_author_name":"GoAndMake.app","gt_author_url":"https://we.goandmake.app","gt_license_name":"CC-BY-NC-4.0","gt_license_url":"https://creativecommons.org/licenses/by-nc/4.0/","gt_theme_name":"Toffee and Coffee","header_class":"bg-primary-color","iconmenu_class":"dn","js_export":{},"js_partial":{},"lightness":45,"lightness_dark":20,"lightness_light":65,"lightness_lighter":80,"logo_url":"/images/logos/icon-on-dark.svg","mainSections":["post"],"main_menu_align":"start","mainsections":["post"],"mobile_menu_button":[{"label":"Menu","menu":"global"},{"label":"Account","menu":"iconmenu"}],"primary_hue":353,"saturation":35,"site_logo_class":"w-100 tc pb3","skip_cover_nav":false};
+gtpb.default.SiteBaseURL = "https://toffee-and-coffee.gitlab.io";
+gtpb.default.SiteCopyright = "Copyright 2018, GoAndMake.app";
+gtpb.default.SiteTitle = "Toffee and Coffee theme demo";
+gtpb.default.SiteFooter = {"appearance":{"legal_link_color":"link-white-80","nav_link_color":"link-gold","section_background":"bg-black-80"},"is_global":true,"menu":{"footer_legal":[{"name":"Terms of Service","url":"#"},{"name":"Privacy","url":"#"}],"footer_nav":[{"name":"Home","url":"#"},{"name":"About","url":"#"}]}};
